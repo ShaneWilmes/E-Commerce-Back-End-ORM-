04ce7aea05d249419cd65c4c74a017628ce9aebf
ShaneWilmes <shanewilmes@yahoo.com>
2022-06-20 21:00:25 -0600

    adding more routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -56,12 +56,12 @@ router.put('/:id', (req, res) => {
         id: req.params.id },
     });
 
-    if (!categoryData) {
+    if (!tagData[0]) {
       res.status(404).json({ message: 'No tag found with this id!' });
       return;
     }
 
-    res.status(200).json(categoryData[0]);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
